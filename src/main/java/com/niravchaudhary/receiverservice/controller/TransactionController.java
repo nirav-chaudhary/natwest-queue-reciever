0a3b236d949a88a4aaf577730f60adfe4b1cf47a
code-hunterx <30567030+code-hunterx@users.noreply.github.com>
2022-03-30 13:39:33 +0530

    integrated controller with service

--- src/main/java/com/niravchaudhary/receiverservice/controller/TransactionController.java
+++ src/main/java/com/niravchaudhary/receiverservice/controller/TransactionController.java
@@ -1,19 +1,28 @@
 package com.niravchaudhary.receiverservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.niravchaudhary.receiverservice.entity.Transaction;
+import com.niravchaudhary.receiverservice.service.TransactionService;
+
 
 @RestController
 @RequestMapping(value = "/transaction")
 public class TransactionController {
 	
+	@Autowired
+	TransactionService transactionService;
+	
 	@PostMapping
 	public ResponseEntity<Transaction> receiveTransaction(@RequestBody Transaction transaction){
+		transactionService.saveTransaction(transaction);
 		return ResponseEntity.ok().build();
 	}
 
