436114b52b34ddf7b4def226d7205c9d3838b915
code-hunterx <30567030+code-hunterx@users.noreply.github.com>
2022-03-30 13:13:41 +0530

    first commit

---
+++ src/test/java/com/niravchaudhary/receiverservice/ReceiverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niravchaudhary.receiverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
